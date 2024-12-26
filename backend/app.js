c8ab3bbf8ec05b354225795f806f0ee1ceb7368f
Camellia Kamli <160346039+camelliakamli@users.noreply.github.com>
2024-12-26 12:20:16 +0100

    DB connection

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+require('dotenv').config(); 
 
 const app = express();
-
+//DATABSE CONNECTION
+mongoose.connect(process.env.DB_URL)
+.then(()=>{
+    console.log("Connected to  DB Successfully");
+}).catch((error)=>{
+    console.log("Connection to DB failed" , error);
+});
 app.get('/', (req, res) =>{
     res.send('Hello World');
 });
 
-app.listen(3000, () =>{
-    console.log("I am listening in port 3000");
+app.listen(8800, () => {
+  console.log("Server listening at port 8800 🚀");
 });
